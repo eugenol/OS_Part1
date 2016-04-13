3f929ad4f6912b613a4c2e833754cc2e0b6f41b3
eugenol <eug@live.co.za>
2016-04-13 20:54:01 +0200

    add comments

--- input_generator.c
+++ input_generator.c
@@ -27,33 +27,36 @@ int main(int argc, char **argv)
     unsigned long int burst_time;
     unsigned int arrival_time;
     unsigned long int priority;
-    FILE * fptr;
     
-    gsl_rng * r; //Create instance of RAndom number generator
+    FILE * fptr;
+    gsl_rng * r; //Instance of random number generator
 
+    //Check if all the arguments have been passed
     if(argc<5)
     {
         printf("Usage: input_generator <number of processes> <average number of process arrivals per unit time> <average burst time per proces> <number of priority levels>");
         return EXIT_FAILURE;
     }
 
+    //Convert arguments to numbers
     num_processes = strtol(argv[1],NULL,10);
     ave_num_proc_per_time = strtol(argv[2],NULL,10);
     ave_burst_time_per_proc = strtol(argv[3],NULL,10);
     num_priority_levels = strtol(argv[4],NULL,10);
         
-    fptr = fopen("input.txt","w");
+    fptr = fopen("input.txt","w"); //Open file for writing
     
     
-    r = gsl_rng_alloc(gsl_rng_mt19937); // (gsl_rng_taus); //Create instance of Random number generator
+    r = gsl_rng_alloc(gsl_rng_mt19937); //Create instance of random number generator // (gsl_rng_taus);
     
+    //Check if rng is created
     if(!r)
     {
         printf("Error: Could not instantiate random number generator.");
         return EXIT_FAILURE; 
     }
 
-    seed = get_seed(); //get seed value   
+    seed = get_seed(); //get seed value from entropy pool  
     gsl_rng_set (r, seed); //Seed the rng
 
     for(long int i = 0; i < num_processes; i++)
